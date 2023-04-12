05f081ce0deab61a704f4bd17958ca1920ce463d
Bryan Rodriguez <Serphp@hotmail.com>
2023-04-11 20:08:08 -0500

    Update navbar & global style & better logo

--- components/Main/Navbar.jsx
+++ components/Main/Navbar.jsx
@@ -17,20 +17,29 @@ export const NavBar = () => {
         <>
 
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark probootstrap-navabr-dark">
-        <div className="container">
+        <div className="">
         <a className="navbar-brand">
-            
         {timer.isRunning ? <h1>{`${minutes}:${seconds}`}</h1> : <h3>Timer <span className='beta'>beta</span></h3>}
-
         </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#probootstrap-nav" aria-controls="probootstrap-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="probootstrap-nav">
-            <ul className="navbar-nav ml-auto">
+        <div className="navcenter">
+            <button className='btn' onClick={toggleTimer}>{timer.isRunning ? 'Pausar' : 'Iniciar'}</button>
+        </div>
+        <div className='navbarconlist navcenter'>
+        <ul className="navbar-nav ml-auto">
             <li className="nav-item"><Link to="/" className="nav-link">Timer</Link></li>
             <li className="nav-item"><Link to="/Taskpage" className="nav-link">List</Link></li>
+            <li className="nav-item">
+                </li>
+            </ul>
+            </div>
+        <div className="navbarlist" id="probootstrap-nav">
+            <ul className="navline">
+            <li className="">
+                <Link to="/" className='navlink' > Home </Link>
+            </li>
+            <li className="">
+                <Link to="/Taskpage" className="navlink">List</Link>
+            </li>
             <li className="nav-item">
                 <a className="nav-link">
                     <button className='btn' onClick={toggleTimer}>{timer.isRunning ? 'Pausar' : 'Iniciar'}</button>
