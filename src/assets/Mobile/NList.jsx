157264f5def2788aa4c336425508b27f7edc6f7d
Bryan Rodriguez <Serphp@hotmail.com>
2023-05-22 21:19:39 -0500

    [Update 1.2.3] Small fix design mobile #3

--- src/assets/Mobile/NList.jsx
+++ src/assets/Mobile/NList.jsx
@@ -1,7 +1,7 @@
 export const NavbarMList = () => {
     return (
         <>
-        <svg width="20" height="20" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <svg width="36" height="36" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path d="M3.5 5.5 5 7l2.5-2.5"></path>
         <path d="M3.5 11.5 5 13l2.5-2.5"></path>
         <path d="M3.5 17.5 5 19l2.5-2.5"></path>
